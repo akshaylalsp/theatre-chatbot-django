f3afa64b96f013c185fa537aa3a5b58cd2aace18
akzaylal <akshaylalsp6@gmail.com>
2024-05-05 18:44:40 +0530

    added views

--- navigation/views.py
+++ navigation/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'index.html')
+    return render(request,'home.html')
+
+@login_required
+def theatre(request):
+    return render(request,'theatre.html')
+
+@login_required
+def location(request):
+    pass
+
+@login_required
+def askme(request):
+    return redirect('http://localhost:8501/')
+
+@login_required
+def showtime(request):
+    return render(request,'home.html')
