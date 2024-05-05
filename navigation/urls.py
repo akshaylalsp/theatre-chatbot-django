0b1478af9b56431ac3ea8fb8ba4f1843dc427d73
akzaylal <akshaylalsp6@gmail.com>
2024-05-05 18:44:22 +0530

    added path

--- navigation/urls.py
+++ navigation/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('theatre/',views.theatre,name='theatre'),
+    path('showtime/',views.showtime,name='showtime'),
+    path('askme/',views.askme,name='askme')
 ]
