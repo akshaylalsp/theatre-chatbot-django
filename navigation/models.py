59d608a7f80cb5d1cd7b573c9521bb29a4d87528
akzaylal <akshaylalsp6@gmail.com>
2024-05-05 18:44:06 +0530

    created model for movie and theatre

--- navigation/models.py
+++ navigation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+    name = models.CharField(max_length=50,primary_key=True)
+    image = models.CharField(max_length=50)
+    inLanguage = models.CharField(max_length=50)
+    duration = models.CharField(max_length=50)
+    datePublished = models.CharField(max_length=50)
+    movie_detail_link = models.CharField(max_length=50)
+    summary = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    casts = models.CharField(max_length=50)
+    rating = models.CharField(max_length=50)
+
+class Theatre(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
