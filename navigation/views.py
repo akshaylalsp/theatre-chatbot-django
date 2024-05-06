a870f37b10bf6939d8850ef63c2d713a79ee5dc9
akzaylal <akshaylalsp6@gmail.com>
2024-05-06 16:45:41 +0530

    added logic

--- navigation/views.py
+++ navigation/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.decorators import login_required
-
+# from .update_movie_table import update_movie
+from .update_movie_table import update_movie
+from .update_theatre_table import update_theatre
+from .models import Theatre,Movies
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'home.html')
+    movies = Movies.objects.all()
+    return render(request,'home.html',{'movies':movies})
 
 @login_required
 def theatre(request):
-    return render(request,'theatre.html')
+    theatres = Theatre.objects.all()
+    return render(request,'theatre.html', {'theatres': theatres})
 
 @login_required
-def location(request):
-    pass
+def location(request,placename):
+    update_movie(placename)
+    update_theatre(placename)
+    return redirect('home')
 
 @login_required
 def askme(request):
